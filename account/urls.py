2b9063ed59fc2128015a1bd9898287902f8c0417
Nikolaos <nickpapanastasis9@gmail.com>
2025-04-23 17:10:08 +0300

    Refactor Code

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,19 @@
-from .views import register, profile, CustomLoginView
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import register, profile
 from django.urls import include, path
 
 app_name = 'account'
 
 urlpatterns = [
-    path('login/', CustomLoginView.as_view(), name='login'),
+    path('login/', LoginView.as_view(
+            template_name='registration/login.html',
+            next_page='account:profile'
+        ), name='login'
+    ),
+    path('logout/', LogoutView.as_view(
+            template_name='registration/login.html',
+            next_page='index'
+        ), name='logout'),
     path('register/', register, name='register'),
     path('profile/', profile, name='profile'),
     path('', include('django.contrib.auth.urls'))
--- account/views.py
+++ account/views.py
@@ -1,16 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from .forms import RegistrationForm, UpdateUserDetails
-from django.contrib.auth.views import LoginView
 from django.shortcuts import render, redirect
-
-class CustomLoginView(LoginView):
-    template_name = 'registration/login.html'
-
-    def dispatch(self, request, *args, **kwargs):
-        if request.user.is_authenticated:
-            return redirect('account:profile')
-        
-        return super().dispatch(request, *args, **kwargs)
 
 # Create your views here.
 def register(request):
@@ -27,7 +17,7 @@ def register(request):
 
         return render(request, 'registration/register.html')
     
-@login_required(login_url='/account/login')
+@login_required(login_url='account:login')
 def profile(request):
     if request.method == 'POST':
         form = UpdateUserDetails(request.POST, instance=request.user)
